6a73bbf66baf470852b404ada4dd58f05623ae44
Lemucya <lemucya@mail.ru>
2022-11-14 20:24:34 +0700

    программа итоговой проверочной работы

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,10 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-string newpath = @"C:\Users\Lemucya\Desktop\FinalVerificationWork112022\Test";
-
-string [] Text = File.ReadAllText(newpath)
-.Split(new char[]{' ', ',', ':', ';','.','!'}
-, StringSplitOptions.RemoveEmptyEntries); 
-foreach (string item in Text)
+﻿/*
+Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равна 3 символа.
+*/ 
+string [] CreateArray(string path)
 {
-    Console.WriteLine(item);
+    string[] Text = File.ReadAllText(path)
+    .Split(new char[] { ' ', ',', ':', ';', '.', '!' }
+    , StringSplitOptions.RemoveEmptyEntries);
+    return Text;
 }
+
+string MainTask(string []InputArray)
+{
+    int count = 0;
+    int index = 0;
+    string[] InputText = new string[0];
+
+    while (count < InputArray.Length)
+    {
+        if (InputArray[count].Length <= 3)
+        {
+            Array.Resize(ref InputText, InputText.Length + 1);
+            InputText[index] = InputArray[count];
+            index++;
+        }
+        count++;
+    }
+string FinalText = String.Join(",", InputText);
+return FinalText;
+}
+
+void WriteArrayInFile(string FinalText)
+{
+    File.WriteAllText(@"C:\Users\Lemucya\Desktop\FinalVerificationWork112022\Test\output.txt", FinalText);
+}
+
+string path = @"C:\Users\Lemucya\Desktop\FinalVerificationWork112022\Test\input.txt";
+
+
+WriteArrayInFile(MainTask(CreateArray(path)));
