c4757c95b7b793f19a211376fdd18e3f52c397c3
Lemucya <lemucya@mail.ru>
2022-11-14 20:10:49 +0700

    Текст в одну строку

---
+++ Test/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string newpath = @"C:\Users\Lemucya\Desktop\FinalVerificationWork112022\Test";
+
+string [] Text = File.ReadAllText(newpath)
+.Split(new char[]{' ', ',', ':', ';','.','!'}
+, StringSplitOptions.RemoveEmptyEntries); 
+foreach (string item in Text)
+{
+    Console.WriteLine(item);
+}
